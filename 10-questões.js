f2839670167301aa373f29111359cdaf1f3671b5
unknown <ramonsantos6053@digitalcollege.internal>
2025-04-24 16:50:15 -0300

    Nome da banda

---
+++ 10-questões.js
@@ -0,0 +1,88 @@
+// Questao 1
+// let idadeMinima = 18;
+
+// if(idadeMinima >= 18){
+//     console.log("Maior de idade");
+// }else{
+//     console.log("menor de idade");
+
+// }
+// Questão 2
+// let numero = -10;
+// if(numero > 0){
+//     console.log("positivo");
+// } else if(numero == 0)
+//     {console.log("Zero");
+// } else{
+//     console.log("negativo");
+// }
+// questão 3
+// let numeroA = 5;
+// let numeroB = 5;
+
+// if (numeroA > numeroB) {
+//     console.log(`${numeroA} È maior que ${numeroB}`);
+// } else if (numeroB > numeroA){
+//     console.log(`${numeroB} é maior que ${numeroA}`);
+// } else {
+//     console.log("os numeros sao iguais");
+// }
+// Questão 4
+// let numero = 21;
+// if((numero % 2) ==0){
+//     console.log(`${numero} è par!`);
+// }else {
+//     console.log(`${numero} é impar!`);
+
+// }
+// Questão 5 
+// let notaA = 5;
+// let notaB = 5;
+// let notaC = 6;
+// let media = ((notaA + notaB + notaC) / 3).toFixed(1);
+
+// if (media >= 7) {
+//     situacao = "aprovado";
+// } else if (media > 5 && media < 7) {
+//     situacao = "Recuperação"
+// }else {
+//     situacao = "Reprovado"
+// }
+// console.log(`${situacao}, media: ${media}`);
+// questão 6
+// let valor = 620;
+// let desconto = 0.35;
+// if (valor >100 ){
+// console.log(`total a pagar R$ ${valor - (valor * desconto).toFixed(4)}`);
+// }else {
+//     console.log(`total a pagar R$ ${valor.toFixed(4)}`);
+// }
+// questão 7
+//Pulado
+// Questão 8 
+// let usuario = "ramon"
+// let senha = 1234
+// if (usuario == "ramon" && senha == 1234 ){
+//     console.log("acesso Liberado");
+// } else{
+//     console.log("acesso negado");
+// }
+// Questao 9
+// let compra = 150;
+// let frete = 20;
+
+// if (compra >= 200) {
+//     console.log(`Frete Grátis R$${compra.toFixed(2)}`);
+// } else {
+//     let total = compra + frete;
+//     console.log(`Total com frete: R$${total.toFixed(2)}`);
+// }
+// Questão 10
+let numero = 60
+if(numero > 10 && numero < 50 )
+    console.log("Esta No Intervalo");
+else{
+    console.log("Fora Do Intervalo");
+}
+    
+
---
+++ atividade-switch-case.js
@@ -0,0 +1,39 @@
+// let pontoCardeal = "leste"
+
+// switch (pontoCardeal) {
+//     case "norte":
+//         console.log("pra cima");
+//         break;
+//     case "sul":
+//         console.log("Pra baixo");
+//         break;
+
+//     case "oeste":
+//         console.log("pra direita");
+//         break;
+//     case "leste":
+//         console.log("Pra esquerda");
+//         break;
+//         default:
+//         console.log("Informe uma opção valida!");
+// }
+// let menuDiario = 5
+// switch (menuDiario) {
+//     case 1:
+//     console.log("peixe");
+    
+//         break;
+//     case 2:
+//     console.log("carne");
+    
+//         break;
+//     case 3:
+//  console.log("frango");
+ 
+//         break;
+
+//     default:
+//         console.log("tem mais ta em falta");
+        
+//         break;
+// }
